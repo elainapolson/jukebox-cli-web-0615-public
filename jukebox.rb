43abb71b22962747566f25b36897fd792f909dca
elainapolson <elainapolson@gmail.com>
2015-06-07 20:34:45 -0400

    done

--- jukebox.rb
+++ jukebox.rb
@@ -11,3 +11,50 @@ songs = [
 ]
 
 # Your code here
+
+
+def play(songs)
+  song = gets.strip
+  if song.to_i >= songs.length
+    puts "Invalid input, please try again"
+  elsif song =~ /\A\d+\z/
+    puts "Playing #{songs}.index(song.to_i)"
+  else 
+    puts "Playing #{song}"
+  end
+end
+
+def help
+  puts "I accept the following commands:"
+  puts "- help: displays this help message"
+  puts "- list: displays a list of songs"
+  puts "- play: lets you choose a song to play"
+  puts "- exit: exits this program"
+end
+
+def list(songs)
+  songs.each_with_index do |song, index|
+    puts "#{index+1}. #{song}"
+  end 
+end 
+
+def exit_jukebox
+  puts "Goodbye"
+end
+
+
+def run(songs)
+  puts "Please enter a command:"
+  command = gets.strip.downcase
+  while command != "exit"
+    if command == "help"
+      help
+    elsif command == "list"
+      list
+    elsif command ==  "play"
+      play
+    end
+    break
+    exit_jukebox
+  end
+end
